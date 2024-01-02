e48faf7471b3df375e6b749402c893f6c7702bd1
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2024-01-02 13:00:38 -0600

    Merge commit 'ed4971cd30b7b25ea0201d3fd50c45c5cb714f09'

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mot-py',
+    version='0.1.0',    
+    description=' Python implementations of multi-object tracking algorithms ',
+    url='https://github.com/ShaneFlandermeyer/MOTpy',
+    author='Shane Flandermeyer',
+    author_email='shaneflandermeyer@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=("test",)),
+    install_requires=[
+      'numpy',
+      'scipy',
+      'matplotlib',
+      'pytest',                     
+    ],
+
+)
