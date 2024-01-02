Fixed Gaussian mixture precision

motpy/distributions/gaussian.py
@@ -51,7 +51,7 @@ def mix_gaussians(means: List[np.ndarray],
   N = len(weights)
   x = np.array(means)
   P = np.array(covars)
-  w = weights / np.sum(weights)
+  w = weights / (np.sum(weights) + 1e-15)
 
   mix_mean = np.dot(w, x)
   mix_covar = np.zeros((x.shape[1], x.shape[1]))

motpy/rfs/momb.py
@@ -147,7 +147,7 @@ class MOMBP:
       in_gate_mb[i, valid_inds] = True
 
       # Create hypotheses with measurement updates
-      if len(valid_inds) > 0:
+      if len(valid_meas) > 0:
         l_mb = np.zeros(m)
         l_mb[valid_inds] = state_estimator.likelihood(
             measurement=valid_meas, predicted_state=bern.state)
@@ -168,8 +168,9 @@ class MOMBP:
       valid_meas, valid_inds = state_estimator.gate(
           measurements=measurements, predicted_state=state, pg=self.pg)
       in_gate_poisson[k, valid_inds] = True
-      l_ppp[k, valid_inds] = state_estimator.likelihood(
-          measurement=valid_meas, predicted_state=state)
+      if len(valid_meas) > 0:
+        l_ppp[k, valid_inds] = state_estimator.likelihood(
+            measurement=valid_meas, predicted_state=state)
 
     for j in range(m):
       bern, wnew[j] = self.poisson.update(

test/test_momb.py
@@ -6,6 +6,7 @@ from motpy.distributions.gaussian import GaussianState
 from motpy.kalman import KalmanFilter
 from motpy.models.measurement import LinearMeasurementModel
 from motpy.models.transition import ConstantVelocity
+import matplotlib.pyplot as plt
 
 
 # def test_predict():
@@ -32,7 +33,7 @@ from motpy.models.transition import ConstantVelocity
 
 def test_scenario():
   """
-  TODO: Test the algorithm with a simple multi-object scenario
+  Test the algorithm with a simple multi-object scenario
   """
   seed = 0
   np.random.seed(seed)
@@ -84,7 +85,7 @@ def test_scenario():
                birth_states=[GaussianState(
                    mean=np.array([0, 0, 0, 0]),
                    covar=np.diag([100, 1, 100, 1])**2)],
-               pg=1,
+               pg=1.0,
                w_min=1e-4,
                r_min=1e-4,
                r_estimate_threshold=0.8)
@@ -94,11 +95,30 @@ def test_scenario():
   kf = KalmanFilter(transition_model=cv, measurement_model=linear)
 
   start = time.time()
+  # plt.ion()
+  # plt.figure()
+
   for k in range(10):
     momb.mb, momb.poisson = momb.predict(state_estimator=kf, dt=dt, ps=0.999)
 
     momb.mb, momb.poisson = momb.update(
         measurements=Z[k], pd=pd, state_estimator=kf, lambda_fa=lambda_c/volume)
+    
+    # plt.clf()
+    # # Plot ground truth up to this point
+    # for path in paths:
+    #   plt.plot([z[0] for z in path[:k+1]], [z[2] for z in path[:k+1]], '-')
+    # plt.plot([z[0] for z in Z[k]], [z[1] for z in Z[k]], 'x')
+    # # Plot markers for MB components over the threshold
+    # for mb in momb.mb:
+    #   if mb.r > momb.r_estimate_threshold:
+    #     plt.plot(mb.state.mean[0], mb.state.mean[2], 'r^')
+    
+    # plt.xlim(-100, 100)
+    # plt.ylim(-100, 100) 
+    # plt.pause(0.01)
+     
+    # plt.draw()
 
   print(f'MOMB: {time.time() - start:.3f} s')
   assert len(momb.mb) == 52