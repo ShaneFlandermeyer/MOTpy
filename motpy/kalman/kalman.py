f52ca599de3efd2d8fcc0eaa1950f0449fabe862
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2024-01-10 07:55:45 -0600

    Tensordict for Kalman Filter class

--- motpy/kalman/kalman.py
+++ motpy/kalman/kalman.py
@@ -6,6 +6,7 @@ from motpy.models.transition.base import TransitionModel
 from motpy.distributions.gaussian import GaussianState
 import motpy.distributions.gaussian as gaussian
 from motpy.gate import EllipsoidalGate
+import torch
 
 
 class KalmanFilter():
@@ -22,51 +23,53 @@ class KalmanFilter():
               ) -> GaussianState:
     assert self.transition_model is not None
 
-    x, P = state.mean, state.covar
-    F = self.transition_model.matrix(dt=dt)
-    Q = self.transition_model.covar(dt=dt)
+    x, P = state['mean'], state['covar']
+    F = torch.tensor(self.transition_model.matrix(dt=dt)).float()
+    Q = torch.tensor(self.transition_model.covar(dt=dt)).float()
 
-    x_pred = F @ x
+    x_pred = x @ F.T
     P_pred = F @ P @ F.T + Q
 
-    # Clear cache from previous update step
-    meta = state.metadata.copy()
-    if 'cache' in meta:
-      for key in ['S', 'K', 'P_post']:
-        meta['cache'].pop(key, None)
+    pred_state = GaussianState(mean=x_pred, covar=P_pred)
+    # Clear cache
+    pred_state['cache'] = {}
 
-    return GaussianState(mean=x_pred, covar=P_pred, metadata=meta)
+    return pred_state
 
   def update(self,
              measurement: np.ndarray,
              predicted_state: GaussianState) -> GaussianState:
     assert self.measurement_model is not None
 
-    x_pred, P_pred = predicted_state.mean, predicted_state.covar
-    z = measurement
-    H = self.measurement_model.matrix()
-    R = self.measurement_model.covar()
+    x_pred, P_pred = predicted_state['mean'], predicted_state['covar']
+    z = torch.tensor(measurement).float()
+    H = torch.tensor(self.measurement_model.matrix()).float()
+    R = torch.tensor(self.measurement_model.covar()).float()
 
     # Use cached values if available
-    cache = predicted_state.metadata.get('cache', {})
-    
-    S = cache['S'] if 'S' in cache else H @ P_pred @ H.T + R
-    K = cache['K'] if 'K' in cache else P_pred @ H.T @ np.linalg.inv(S)
-    if 'P_post' in cache:
+    cache = predicted_state.get('cache', None)
+
+    if cache is not None:
+      S = cache['S']
+      K = cache['K']
       P_post = cache['P_post']
     else:
-      P_post = P_pred - K @ S @ K.T
-      P_post = (P_post + P_post.T) / 2
-      
-    z_pred = H @ x_pred
-    x_post = x_pred + K @ (z - z_pred) if z is not None else None
+      S = H @ P_pred @ H.T + R
+      K = P_pred @ H.T @ np.linalg.inv(S)
+      P_post = P_pred - K @ S @ K.mT
+      P_post = (P_post + P_post.mT) / 2
+
+      # Update cache
+      cache = dict(S=S, K=K, P_post=P_post)
+
+    z_pred = x_pred @ H.T
+    x_post = x_pred + (z - z_pred) @ K.mT
 
     # Update cache
-    meta = predicted_state.metadata
-    meta['cache'] = meta.get('cache', {})
-    meta['cache'].update(dict(S=S, K=K, P_post=P_post))
+    post_state = GaussianState(mean=x_post, covar=P_post)
+    post_state['cache'] = cache
 
-    return GaussianState(mean=x_post, covar=P_post, metadata=meta.copy())
+    return post_state
 
   def gate(self,
            measurements: List[np.ndarray],
--- test/test_kalman.py
+++ test/test_kalman.py
@@ -4,6 +4,7 @@ from filterpy.kalman import predict, update
 
 from motpy.distributions.gaussian import GaussianState
 from motpy.kalman import KalmanFilter
+import torch
 
 
 class TestLinearTransitionModel():
@@ -38,20 +39,21 @@ def test_kalman_predict():
   """
   Test kalman predict step. Example data from kalman filter ebook.
   """
-  state = GaussianState(mean=np.array([11.35, 4.5]),
-                        covar=np.array([[545, 150], [150, 500]]))
+  state = GaussianState(mean=torch.Tensor([11.35, 4.5]),
+                        covar=torch.Tensor([[545, 150], [150, 500]]))
   dt = 0.3
   transition_model = TestLinearTransitionModel()
   F = transition_model.matrix(dt)
   Q = transition_model.covar()
-  
+
   kf = KalmanFilter(
-    transition_model=TestLinearTransitionModel(),
-    measurement_model=None)
+      transition_model=TestLinearTransitionModel(),
+      measurement_model=None)
   state_pred = kf.predict(state=state, dt=dt)
-  x_expected, P_expected = predict(x=state.mean, P=state.covar, F=F, Q=Q)
-  assert np.allclose(state_pred.mean, x_expected)
-  assert np.allclose(state_pred.covar, P_expected)
+  x_expected, P_expected = predict(
+      x=state[0]['mean'], P=state[0]['covar'], F=F, Q=Q)
+  assert np.allclose(state_pred['mean'], x_expected)
+  assert np.allclose(state_pred['covar'], P_expected)
 
 
 def test_kalman_update():
@@ -62,17 +64,19 @@ def test_kalman_update():
   R = measurement_model.covar()
   H = measurement_model.matrix()
   z = 1
-  state = GaussianState(mean=np.array([12.7, 4.5]),
-                        covar=np.array([[545, 150], [150, 500]]))
+  state = GaussianState(mean=torch.Tensor([12.7, 4.5]),
+                        covar=torch.Tensor([[545, 150], [150, 500]]))
+  # state = torch.cat((state, state.clone()))
   kf = KalmanFilter(
-    transition_model=TestLinearTransitionModel(),
-    measurement_model=TestLinearMeasurementModel(),
+      transition_model=TestLinearTransitionModel(),
+      measurement_model=TestLinearMeasurementModel(),
   )
-  
+
   state_post = kf.update(measurement=z, predicted_state=state)
-  x_expected, P_expected = update(x=state.mean, P=state.covar, z=z, R=R, H=H)
-  assert np.allclose(state_post.mean, x_expected)
-  assert np.allclose(state_post.covar, P_expected)
+  x_expected, P_expected = update(
+      x=state[0]['mean'], P=state[0]['covar'], z=z, R=R, H=H)
+  assert np.allclose(state_post['mean'], x_expected)
+  assert np.allclose(state_post['covar'], P_expected)
 
 
 if __name__ == '__main__':
