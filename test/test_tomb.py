f7ac6d162000ae1ca42f81ea6d2397e443f47785
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2024-02-02 13:18:40 -0600

    Added TOMB test with gating active

--- test/test_tomb.py
+++ test/test_tomb.py
@@ -145,6 +145,52 @@ def test_scenario_merge():
   assert np.allclose(tracker.mb[0].r, 0.9999935076418562, atol=1e-6)
   assert np.allclose(tracker.mb[3].r, 0.9999901057591115, atol=1e-6)
 
+def test_scenario_gate():
+  """
+  Test the algorithm with a simple multi-object scenario
+  """
+
+  def pd(x): return 0.8
+  def ps(x): return 0.999
+  lambda_c = 20
+  dt = 1
+  n_steps = 10
+  volume = 200*200
+  paths, Z, cv, linear = make_data(
+      dt=dt, lambda_c=lambda_c, pd=pd, n_steps=n_steps)
+
+  # Initialize TOMB filter
+  birth_dist = GaussianState(
+      mean=np.array([0, 0, 0, 0]),
+      covar=np.diag([100, 1, 100, 1])**2,
+      weight=np.array([0.05]),
+  )
+  init_dist = GaussianState(
+      mean=birth_dist.mean,
+      covar=birth_dist.covar,
+      weight=[10.0])
+  tracker = TOMBP(birth_distribution=birth_dist,
+                  pg=0.999,
+                  w_min=None,
+                  merge_poisson=True,
+                  r_min=1e-4,
+                  r_estimate_threshold=0.5,
+                  )
+  tracker.poisson.distribution = init_dist
+
+  kf = KalmanFilter(transition_model=cv, measurement_model=linear)
+
+  for k in range(n_steps):
+    tracker.mb, tracker.poisson = tracker.predict(
+        state_estimator=kf, dt=dt, ps_func=ps)
+
+    tracker.mb, tracker.poisson = tracker.update(
+        measurements=Z[k], pd_func=pd, state_estimator=kf, lambda_fa=lambda_c/volume)
+
+  assert len(tracker.mb) == 53
+  assert len(tracker.poisson) == 1
+  assert np.allclose(tracker.mb[0].r, 0.9999935076418562, atol=1e-6)
+  assert np.allclose(tracker.mb[3].r, 0.9999901057591115, atol=1e-6)
 
 if __name__ == '__main__':
   # test_scenario_prune()
