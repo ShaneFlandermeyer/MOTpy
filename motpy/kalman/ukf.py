78f5993616aa154e44fa5e7348ebd327ec5b4d16
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2024-11-21 15:35:05 -0600

    Simplify UKF tests and interface

--- motpy/kalman/ukf.py
+++ motpy/kalman/ukf.py
@@ -23,7 +23,9 @@ class UnscentedKalmanFilter():
   def predict(self,
               state: Gaussian,
               dt: float,
-              filter_state: Dict[str, Any] = dict(),
+              sigma_points: np.ndarray,
+              Wm: np.ndarray,
+              Wc: np.ndarray,
               ) -> Tuple[Gaussian, Dict]:
     """
     UKF predict step
@@ -34,11 +36,12 @@ class UnscentedKalmanFilter():
         Object states
     dt : float
         Prediction time step
-    filter_state : Dict[str, Any]
-        Filter state dictionary. This dict should contain the following keys:
-        - alpha: Merwe scaling parameter
-        - beta: Merwe scaling parameter
-        - kappa: Merwe scaling parameter
+    sigma_points : np.ndarray
+        2n + 1 x n matrix of sigma points
+    Wm : np.ndarray
+        Mean weights for the unscented transform
+    Wc : np.ndarray
+        Covariance weights for the unscented transform
 
     Returns
     -------
@@ -50,30 +53,11 @@ class UnscentedKalmanFilter():
             - Wm: Mean weights for the unscented transform
             - Wc: Covariance weights for the unscented transform
     """
-    # Extract filter state variables
-    alpha = filter_state['alpha']
-    beta = filter_state['beta']
-    kappa = filter_state['kappa']
-
-    # Compute sigma points and weights
-    sigmas = merwe_scaled_sigma_points(
-        x=state.mean,
-        P=state.covar,
-        alpha=alpha,
-        beta=beta,
-        kappa=kappa,
-        subtract_fn=self.state_residual_fn)
-    Wm, Wc = merwe_sigma_weights(
-        ndim_state=state.mean.shape[-1],
-        alpha=alpha,
-        beta=beta,
-        kappa=kappa)
-
     # Transform sigma points to the prediction space
-    predicted_sigams = self.transition_model(sigmas, dt=dt, noise=False)
+    predicted_sigmas = self.transition_model(sigma_points, dt=dt)
 
-    predicted_mean, predicted_covar = self.unscented_transform(
-        sigmas=predicted_sigams,
+    predicted_mean, predicted_covar = unscented_transform(
+        sigmas=predicted_sigmas,
         Wm=Wm,
         Wc=Wc,
         noise_covar=self.transition_model.covar(dt=dt),
@@ -82,17 +66,17 @@ class UnscentedKalmanFilter():
     predicted_state = Gaussian(
         mean=predicted_mean, covar=predicted_covar, weight=state.weight)
 
-    filter_state.update(
-        predicted_sigmas=predicted_sigams,
-        Wm=Wm,
-        Wc=Wc,
+    filter_state = dict(
+        predicted_sigmas=predicted_sigmas
     )
     return predicted_state, filter_state
 
   def update(self,
              predicted_state: Gaussian,
              measurement: np.ndarray,
-             filter_state: Dict[str, Any],
+             predicted_sigmas: np.ndarray,
+             Wm: np.ndarray,
+             Wc: np.ndarray,
              ) -> Tuple[Gaussian, Dict]:
     """
     UKF update step
@@ -103,29 +87,24 @@ class UnscentedKalmanFilter():
         Predicted state
     measurement : np.ndarray
         Measurement vectors
-    filter_state : Dict[str, Any]
-        Filter state dictionary. This dict should contain the following keys:
-        - predicted_sigmas: Sigma points in prediction space
-        - Wm: Mean weights for the unscented transform
-        - Wc: Covariance weights for the unscented transform
+    predicted_sigmas : np.ndarray 
+        2n + 1 x n matrix of sigma points in prediction space
+    Wm : np.ndarray
+        Mean weights for the unscented transform
+    Wc : np.ndarray
+        Covariance weights for the unscented transform
 
     Returns
     -------
     Tuple[GaussianState, Dict]
         A tuple containing the:
-        - Predicted state object
-        - Filter state dict with the following updated keys:
-            - measured_sigmas: Sigma points in measurement space
+        - Predicted state
+        - Filter state dict with sigma points in measurement space
     """
 
-    # Extract state variables
-    predicted_sigmas = filter_state['predicted_sigmas']
-    Wm = filter_state['Wm']
-    Wc = filter_state['Wc']
-
     # Unscented transform in measurement space
-    measured_sigmas = self.measurement_model(predicted_sigmas, noise=False)
-    z_pred, S = UnscentedKalmanFilter.unscented_transform(
+    measured_sigmas = self.measurement_model(predicted_sigmas)
+    z_pred, S = unscented_transform(
         sigmas=measured_sigmas,
         Wm=Wm,
         Wc=Wc,
@@ -136,7 +115,7 @@ class UnscentedKalmanFilter():
     # Standard kalman update
     x_pred, P_pred = predicted_state.mean, predicted_state.covar
     z = measurement
-    Pxz = np.einsum('k, ...ki, ...kj -> ...ij',
+    Pxz = np.einsum('...n, ...ni, ...nj -> ...ij',
                     Wc,
                     self.state_residual_fn(predicted_sigmas, x_pred),
                     self.measurement_residual_fn(measured_sigmas, z_pred))
@@ -144,27 +123,28 @@ class UnscentedKalmanFilter():
     K = Pxz @ np.linalg.inv(S)
     x_post = x_pred + np.einsum('...ij, ...j -> ...i', K, y)
     P_post = P_pred - K @ S @ K.swapaxes(-1, -2)
-    P_post = (P_post + P_post.swapaxes(-1, -2)) / 2
+    P_post = 0.5*(P_post + P_post.swapaxes(-1, -2))
     post_state = Gaussian(
-        mean=x_post, covar=P_post, weight=predicted_state.weight)
+        mean=x_post, covar=P_post, weight=predicted_state.weight
+    )
 
-    filter_state.update(
+    filter_state = dict(
         measured_sigmas=measured_sigmas,
     )
     return post_state, filter_state
 
-  @staticmethod
-  def unscented_transform(sigmas: np.ndarray,
-                          Wm: np.ndarray,
-                          Wc: np.ndarray,
-                          noise_covar: np.ndarray,
-                          residual_fn: callable = np.subtract,
-                          ) -> Tuple[np.ndarray, np.ndarray]:
-    # Mean computation
-    x = np.dot(Wm, sigmas)
 
-    # Covariance computation
-    y = residual_fn(sigmas, x[..., None, :])
-    P = np.einsum('k, ...ki, ...kj -> ...ij', Wc, y, y) + noise_covar
+def unscented_transform(sigmas: np.ndarray,
+                        Wm: np.ndarray,
+                        Wc: np.ndarray,
+                        noise_covar: np.ndarray,
+                        residual_fn: callable = np.subtract,
+                        ) -> Tuple[np.ndarray, np.ndarray]:
+  # Mean
+  x = np.einsum('...n, ...ni -> ...i', Wm, sigmas)
 
-    return x, P
+  # Covariance
+  y = residual_fn(sigmas, x[..., None, :])
+  P = np.einsum('...n, ...ni, ...nj -> ...ij', Wc, y, y) + noise_covar
+
+  return x, P
--- test/test_ukf.py
+++ test/test_ukf.py
@@ -1,36 +1,44 @@
-import numpy as np
-
-import pytest
-from motpy.models.transition import ConstantVelocity
-from motpy.kalman import UnscentedKalmanFilter
-from motpy.distributions.gaussian import Gaussian
 import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+from filterpy.kalman import UnscentedKalmanFilter as UKF
+from filterpy.kalman.sigma_points import MerweScaledSigmaPoints
+
+from motpy.distributions.gaussian import Gaussian
+from motpy.kalman import UnscentedKalmanFilter
+from motpy.kalman.sigma_points import (merwe_scaled_sigma_points,
+                                       merwe_sigma_weights)
 from motpy.models.measurement.range_bearing import RangeBearingModel
+from motpy.models.transition import ConstantVelocity
 
 
 def test_predict():
-  seed = 0
 
+  seed = 0
+  dt = 1
+  w = 0.01
+  alpha = 0.1
+  beta = 2
+  kappa = 0
   state = Gaussian(
       mean=np.array([0, 1, 0, 1]),
       covar=np.diag([1., 0.5, 1., 0.5]))
+  state_dim = state.state_dim
 
   # Motpy UKF
-  w = 0.01
-  cv = ConstantVelocity(ndim_state=4, w=w, seed=seed)
+  cv = ConstantVelocity(ndim_state=state_dim, w=w, seed=seed)
   ukf = UnscentedKalmanFilter(transition_model=cv, measurement_model=None)
-  filter_state = {
-      'alpha': 0.1,
-      'beta': 2,
-      'kappa': 0
-  }
-  dt = 1
+  points = merwe_scaled_sigma_points(
+      x=state.mean, P=state.covar, alpha=alpha, beta=beta, kappa=kappa
+  )
+  Wm, Wc = merwe_sigma_weights(
+      ndim_state=state_dim, alpha=alpha, beta=beta, kappa=kappa
+  )
   pred_state, filter_state = ukf.predict(
-      state=state, dt=dt, filter_state=filter_state)
+      state=state, dt=dt, sigma_points=points, Wm=Wm, Wc=Wc
+  )
 
   # Filterpy UKF
-  from filterpy.kalman import UnscentedKalmanFilter as UKF
-  from filterpy.kalman.sigma_points import MerweScaledSigmaPoints
   points = MerweScaledSigmaPoints(n=4, alpha=0.1, beta=2, kappa=0)
   ukf_filterpy = UKF(dim_x=4, dim_z=0, dt=dt, hx=None, fx=cv, points=points)
   ukf_filterpy.x = state.mean
@@ -43,116 +51,55 @@ def test_predict():
 
 
 def test_update():
-  seed = 0
+  alpha = 0.1
+  beta = 2
+  kappa = 0
   state = Gaussian(
       mean=np.array([0, 1, 0, 1]),
       covar=np.diag([1., 0.5, 1., 0.5]))
+  state_dim = state.state_dim
   ground_truth = np.array([0, 1, 0, 1])
-
-  # Motpy UKF
   R = np.diag([0.1, np.deg2rad(0.1)])
-  cv = ConstantVelocity(ndim_state=4, w=0, seed=seed)
-  range_bearing = RangeBearingModel(covar=R, seed=seed)
+  range_bearing = RangeBearingModel(covar=R)
+  cv = ConstantVelocity(ndim_state=state_dim, w=0)
+  z = range_bearing(ground_truth, noise=False)
+  
+  pred_sigmas = merwe_scaled_sigma_points(
+      x=state.mean, P=state.covar, alpha=alpha, beta=beta, kappa=kappa
+  )
+  Wm, Wc = merwe_sigma_weights(
+      ndim_state=state_dim, alpha=alpha, beta=beta, kappa=kappa
+  )
+  
+  # Motpy UKF
   ukf = UnscentedKalmanFilter(
-      transition_model=cv, measurement_model=range_bearing)
-  filter_state = {
-      'alpha': 0.1,
-      'beta': 2,
-      'kappa': 0
-  }
-  dt = 0
-
-  # Get the appropriate state dict from prediction
-  pred_state, filter_state = ukf.predict(
-      state=state, dt=dt, filter_state=filter_state)
-  z = range_bearing(ground_truth, noise=True)
-  pred_state, filter_state = ukf.update(
-      predicted_state=pred_state, measurement=z, filter_state=filter_state)
+      transition_model=None,
+      measurement_model=range_bearing
+  )
+  motpy_update, update_state = ukf.update(
+      predicted_state=state,
+      measurement=z,
+      predicted_sigmas=pred_sigmas,
+      Wm=Wm,
+      Wc=Wc
+  )
   
   # Filterpy UKF
-  from filterpy.kalman import UnscentedKalmanFilter as UKF
-  from filterpy.kalman.sigma_points import MerweScaledSigmaPoints
-  points = MerweScaledSigmaPoints(n=4, alpha=0.1, beta=2, kappa=0)
-  ukf_filterpy = UKF(dim_x=4, dim_z=2, dt=dt, hx=range_bearing, fx=cv, points=points)
-  ukf_filterpy.x = state.mean
-  ukf_filterpy.P = state.covar
-  ukf_filterpy.Q = cv.covar(dt=dt)
-  ukf_filterpy.R = R
-  ukf_filterpy.predict()
-  ukf_filterpy.update(z)
-
-  assert np.allclose(pred_state.mean, ukf_filterpy.x)
-  assert np.allclose(pred_state.covar, ukf_filterpy.P)
-  
+  points = MerweScaledSigmaPoints(
+      n=state_dim, alpha=alpha, beta=beta, kappa=kappa)
+  filterpy_update = UKF(dim_x=4, dim_z=2, dt=0, fx=None,
+                        hx=range_bearing, points=points)
+  filterpy_update.x = state.mean
+  filterpy_update.P = state.covar
+  filterpy_update.R = R
+  filterpy_update.sigmas_f = pred_sigmas
+  filterpy_update.update(z)
 
 
-# def test_ukf():
-#   # Generate ground truth
-#   n_steps = 20
-#   current_time = last_update = 0
-#   dt = 1
-
-#   # Generate ground truth
-#   trajectory = []
-#   init_mean, init_covar = np.array([0, 1, 0, 1]), np.diag([1., 0.5, 1., 0.5])
-#   trajectory.append(init_mean)
-#   cv = ConstantVelocity(ndim=2, w=0.05)
-#   for i in range(n_steps):
-#     state = cv(trajectory[-1], dt=dt, noise=False)
-#     trajectory.append(state)
-
-#   # Generate measurements
-#   range_bearing = NonlinearMeasurementModel(R=np.diag([np.deg2rad(0.1), 0.1]))
-#   measurements = []
-#   for state in trajectory:
-#     z = range_bearing(state, noise=False)
-#     measurements.append(z)
-
-#   # Test the UKF
-#   ukf = UnscentedKalmanFilter(
-#       transition_model=cv, measurement_model=range_bearing)
-#   filter_state = {
-#       'alpha': 0.1,
-#       'beta': 2,
-#       'kappa': 0
-#   }
-#   track_states = [GaussianState(mean=init_mean, covar=init_covar)]
-#   pred_state, filter_state = ukf.predict(
-#       state=track_states[-1],
-#       dt=0,
-#       filter_state=filter_state)
-#   for i, m in enumerate(measurements):
-#     post_state, filter_state = ukf.update(
-#         predicted_state=pred_state,
-#         measurement=m,
-#         filter_state=filter_state)
-#     pred_state, filter_state = ukf.predict(
-#         state=post_state, dt=dt, filter_state=filter_state)
-#     track_states.append(post_state)
-
-#   true_states = np.stack([state for state in trajectory]).T
-#   track_states = np.stack([state.mean for state in track_states])[1:].T
-#   track_pos = track_states[[0, 2], 0]
-#   track_vel = track_states[[1, 3], 0]
-#   pos_mse = np.mean(np.linalg.norm(true_states[[0, 2]] - track_pos, axis=1))
-#   vel_mse = np.mean(np.linalg.norm(true_states[[1, 3]] - track_vel, axis=1))
-#   assert pos_mse < 0.2
-#   assert vel_mse < 0.1
+  assert np.allclose(motpy_update.mean, filterpy_update.x)
+  assert np.allclose(motpy_update.covar, filterpy_update.P)
 
 
-# def test_linear_predict():
-#   seed = 0
-#   np.random.seed(seed)
-#   cv = ConstantVelocity(ndim=2, w=0.05, seed=seed)
-#   ukf = UnscentedKalmanFilter(transition_model=cv, measurement_model=None)
-#   kf = KalmanFilter(transition_model=cv, measurement_model=None)
-#   state = GaussianState(mean=np.random.uniform(size=4),
-#                         covar=np.diag(np.random.uniform(size=4)))
-#   dt = 1
-#   kf_pred, meta = kf.predict(state, dt)
-#   ukf_pred, meta = ukf.predict(state, dt)
-#   assert np.allclose(kf_pred.mean, ukf_pred.mean)
-#   assert np.allclose(kf_pred.covar, ukf_pred.covar)
 if __name__ == '__main__':
-  # test_update()
+  test_update()
   pytest.main([__file__])
