9c2fe495667d0d08b21c4501a5a432d524a71a28
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2025-08-06 09:56:47 -0500

    Updated 2D radar measurement model

--- motpy/models/measurement/spherical.py
+++ motpy/models/measurement/spherical.py
@@ -6,69 +6,104 @@ import numpy as np
 from motpy.models.measurement import MeasurementModel
 
 
-class CartesianToSpherical2D(MeasurementModel):
+def normalize_angle(x: np.ndarray) -> np.ndarray:
+  """Normalize angles to the range [-pi, pi]."""
+  return np.mod(x + np.pi, 2 * np.pi) - np.pi
+
+
+class Radar2D(MeasurementModel):
   def __init__(self,
                covar: np.ndarray,
                pos_inds: List[int] = [0, 2],
                vel_inds: List[int] = [1, 3],
-               seed: int = np.random.randint(0, 2**32-1),
                ):
-    # TODO: Some noise covars depend on the sensor state
+    """
+    2D radar measurement model.
+
+    Converts a cartesian input state to [range, azimuth, radial velocity] measurements.
+
+    Parameters
+    ----------
+    covar : np.ndarray
+        Sensor noise covariance matrix.
+    pos_inds : List[int], optional
+        Position state vector indices, by default [0, 2]
+    vel_inds : List[int], optional
+        Velocity state vector indices, by default [1, 3]
+    """
     self.noise_covar = np.array(covar, dtype=float)
     self.pos_inds = pos_inds
     self.vel_inds = vel_inds
-
-    self.np_random = np.random.RandomState(seed)
+    self.measurement_dim = 3  # range, azimuth, radial velocity
 
   def __call__(self,
                x: np.ndarray,
-               sensor_state: np.ndarray,
-               noise: bool = False
+               sensor_pos: np.ndarray,
+               sensor_vel: np.ndarray,
+               noise: bool = False,
+               rng: Optional[np.random.RandomState] = None,
+               **kwargs,
                ) -> np.ndarray:
-    sensor_pos = sensor_state[..., self.pos_inds]
-    sensor_vel = sensor_state[..., self.vel_inds]
-
     rel_pos = x[..., self.pos_inds] - sensor_pos
     rel_vel = x[..., self.vel_inds] - sensor_vel
 
-    az = np.arctan2(rel_pos[..., 1], rel_pos[..., 0])
-    az = np.mod(az + np.pi, 2*np.pi) - np.pi
     r = np.linalg.norm(rel_pos, axis=-1)
-    # Negative by radar convention
+    az = normalize_angle(np.arctan2(rel_pos[..., 1], rel_pos[..., 0]))
     v = -np.einsum('...i, ...i->...', rel_pos, rel_vel) / (r + 1e-15)
-
-    measurement = np.stack([az, r, v], axis=-1)
+    measurement = np.stack([r, az, v], axis=-1)
 
     if noise:
-      measurement += self.sample_noise(size=measurement.shape[:-1])
+      measurement += self.sample_noise(size=measurement.shape[:-1], rng=rng)
 
     return measurement
 
   def covar(self):
     return self.noise_covar
 
-  def sample_noise(self, size: Tuple[int, ...]) -> np.ndarray:
-    measurement_dim = 3
-    noise = self.np_random.multivariate_normal(
-        mean=np.zeros(measurement_dim), cov=self.noise_covar, size=size
+  def sample_noise(
+      self,
+      size: Tuple[int, ...],
+      rng: Optional[np.random.RandomState] = None
+  ) -> np.ndarray:
+    if rng is None:
+      rng = np.random.default_rng()
+
+    noise = rng.multivariate_normal(
+        mean=np.zeros(self.measurement_dim), cov=self.noise_covar, size=size
     )
     return noise
 
   @staticmethod
   def subtract_fn(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-    x = a - b
-    x[..., 0] = np.mod(x[..., 0] + np.pi, 2*np.pi) - np.pi
+    y = a - b
+    y[..., 1] = normalize_angle(y[..., 1])
 
-    return x
+    return y
 
   @staticmethod
-  def mean_fn(x: np.ndarray, w: np.ndarray) -> np.ndarray:
-    w = np.abs(w) / np.abs(w).sum()
-    out = np.zeros(x.shape[:-2] + (x.shape[-1],))
-    out[..., 0] = np.arctan2(
-        np.sum(w * np.sin(x[..., 0]), axis=-1),
-        np.sum(w * np.cos(x[..., 0]), axis=-1)
+  def mean_fn(z: np.ndarray, w: np.ndarray) -> np.ndarray:
+    w = np.abs(w) / (np.abs(w).sum() + 1e-15)
+    az_mean = np.arctan2(
+        np.sum(w * np.sin(z[..., 1]), axis=-1),
+        np.sum(w * np.cos(z[..., 1]), axis=-1)
     )
-    out[..., 1:] = np.sum(w[..., None] * x[..., 1:], axis=-2)
+    r_mean = np.sum(w * z[..., 0], axis=-1)
+    v_mean = np.sum(w * z[..., 2], axis=-1)
 
-    return out
+    return np.stack([r_mean, az_mean, v_mean], axis=-1)
+
+
+if __name__ == '__main__':
+  radar2d = Radar2D(covar=np.eye(3))
+
+  x = np.array([10, 1, 10, 1])
+  z = radar2d(
+      x=x,
+      sensor_pos=np.array([0, 0]),
+      sensor_vel=np.array([0, 0]),
+      noise=False,
+      rng=None,
+  )
+  zm = radar2d.mean_fn(z=np.stack([z, z], axis=0), w=np.array([0.5, 0.5]))
+  print(zm-z)
+  
